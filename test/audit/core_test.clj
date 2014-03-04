ad79532f1f5fa066cbeac4ebbe0e49a91f7dee98
cursive code <cursivecode@gmail.com>
2014-03-04 05:37:24 -0500

    Wrote tests for audit

--- test/audit/core_test.clj
+++ test/audit/core_test.clj
@@ -2,6 +2,63 @@
   (:require [clojure.test :refer :all]
             [audit.core :refer :all]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(deftest regex-test
+  (testing "returns a function"
+    (is (= (function? (regex #"\d+")) true)))
+  (testing "does regex operations"
+    (is (= ((regex #"\d+") "500") "500"))))
+
+(deftest truthy-test
+  (testing "turns truthy into true"
+    (is (= (truthy 1) true))
+    (is (= (truthy "1") true))
+    (is (= (truthy :1) true))
+    (is (= (truthy []) true))
+    (is (= (truthy {}) true))
+    (is (= (truthy #{}) true))
+    (is (= (truthy '()) true)))
+  (testing "turns falsy into false"
+    (is (= (truthy false) false))
+    (is (= (truthy nil) false))))
+
+(deftest audit-helper-test 
+  (let [value-map {:valid-audit true :happy "sad"}
+        audit-map [:happy [string? (regex #"\w")]]
+        audit-map2 [:happy [string? (regex #"\d")]]
+        audit-map3 [:happy [string? (regex #"\w") number?]]
+        ]
+    (testing "keeps vaild-audit to true"
+      (is (= (:valid-audit (audit-helper value-map audit-map))
+             true)))
+    (testing "changes valid-audit to false"
+      (is (= (:valid-audit (audit-helper value-map audit-map2))
+             false)))
+    (testing "multiple functions are ran"
+      (is (= (:valid-audit (audit-helper value-map audit-map3))
+             false)))
+    (testing "should tell the reason for failure"
+      (is (= (:failure-reason (audit-helper value-map audit-map3))
+             ":happy with input sad")))))
+
+(deftest audit-test
+  (let [value-map {:url "http://www.google.com"
+                   :title "the number 1"
+                   :time 400}
+        audit-map {:url [string? (regex #"http://www.")]
+                   :title [string? (regex #"\d")]
+                   :time [number? #(> % 300)]}
+        good-audit (audit audit-map value-map)
+        bad-audit (audit audit-map (assoc value-map :time 200))]
+    (testing "an expection on key amounts"
+      (is (= (try
+               (audit {:happy "you"} {})
+               (catch Exception e :exception-thrown))
+             :exception-thrown)))
+    (testing "good input"
+      (is (= (map? good-audit) true))
+      (is (= (:valid-audit good-audit) true))
+      (is (= (:failure-reason good-audit) nil)))
+    (testing "bad input"
+      (is (= (map? bad-audit) true))
+      (is (= (:valid-audit bad-audit) false))
+      (is (= (:failure-reason bad-audit) ":time with input 200")))))
